89f03db2b9a6b32ec8a034e1bd0f59037d763ffb
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-22 13:15:05 -0300

    Criação de um projeto para instalar lib publicada de teste.

---
+++ UsingBibliotecaTeste/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatematicaOperacao;
+
+namespace UsingBibliotecaTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Soma.OpSoma(2, 2));
+        }
+    }
+}
